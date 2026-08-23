2d3616a48b2a47bb624368088852844ca9305437
Deontre Benauki <124494291+deontre8816rfbnk@users.noreply.github.com>
2026-08-23 11:40:26 +0100

    Implement service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'phrases-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  'https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300..900;1,9..144,300..900&family=DM+Sans:opsz,wght@9..40,400..700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
